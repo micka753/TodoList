4b6efb3759082503f279cd3c5b56cd322d1082ff
micka753 <mcharfad@scinfe141.univ-pau.fr>
2017-11-22 16:57:12 +0100

    Initial commit

---
+++ app/src/main/java/todolist/charfadi/com/todolist/MainActivity.java
@@ -0,0 +1,13 @@
+package todolist.charfadi.com.todolist;
+
+import android.app.Activity;
+import android.os.Bundle;
+//https://code.tutsplus.com/tutorials/android-from-scratch-understanding-adapters-and-adapter-views--cms-26646
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
